6664e02a1a3be3e0ff93cb75254730afdcc56591
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-21 15:56:24 +0530

    Define a DatabaseModel and make migrations

--- my_site_project/job_form/models.py
+++ my_site_project/job_form/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Applicants(models.Model):
+    occupation = models.TextChoices('occupation', "Employed Unemployed Self-employed Student")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    start_date = models.DateField()
+    employment_status = models.CharField(choices=occupation.choices, max_length=15)
